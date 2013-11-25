243520de74ff10bee8a96a071d2f2cf5077cbe65
Kiril Vladimirov <kiril@vladimiroff.org>
2013-11-25 17:32:26 +0200

    Add 3th homework

---
+++ 2013/03/solution_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func loadTheReadme() string {
+	content, err := ioutil.ReadFile("./README.md")
+	if err != nil {
+		return ""
+	}
+	return string(content)
+}
+
+func TestHeaders(t *testing.T) {
+	mdParser := NewMarkdownParser(loadTheReadme())
+	headers := mdParser.Headers()
+
+	if headers[0] != "type MarkdownParser" {
+		t.Fail()
+	}
+}
+
+func TestSubHeadersOf(t *testing.T) {
+	mdParser := NewMarkdownParser(loadTheReadme())
+	subHeaders := mdParser.SubHeadersOf("Пример")
+	if len(subHeaders) != 0 {
+		t.Fail()
+	}
+}
+
+func TestTableOfContents(t *testing.T) {
+	mdParser := NewMarkdownParser(loadTheReadme())
+	tableOfContents := mdParser.GenerateTableOfContents()
+
+	if strings.Split(tableOfContents, "\n")[4] != "1.1.3 `func (mp *MarkdownParser) SubHeadersOf(header string) []string`" {
+		t.Fail()
+	}
+}
