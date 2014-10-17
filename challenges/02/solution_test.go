020b1b65ab021c0633501b838f30360238523fa4
Doychin Atanasov <ironsmile@gmail.com>
2014-10-17 21:39:02 +0300

    Actually adding some tests

--- challenges/02/solution_test.go
+++ challenges/02/solution_test.go
@@ -24,3 +24,13 @@ func checkFail(t *testing.T, resultWords, expectedWords []string) {
 		t.Errorf("Function returned uninitialized slice!")
 	}
 }
+
+func TestWithExampleData(t *testing.T) {
+	text := "A Be Ce DE De a! oh? a oy! oY! OY!"
+
+	checkFail(t, getMostCommonWords(text, 4), []string{})
+	checkFail(t, getMostCommonWords(text, 3), []string{"oy!"})
+	checkFail(t, getMostCommonWords(text, 2), []string{"a", "de", "oy!"})
+	checkFail(t, getMostCommonWords(text, 1), []string{"a", "a!", "be", "ce", "de", "oh?", "oy!"})
+	checkFail(t, getMostCommonWords(text, 0), []string{"a", "a!", "be", "ce", "de", "oh?", "oy!"})
+}
