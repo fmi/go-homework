18e907f04f9a51c0ea0aff59e143833497cbfc6c
pavelhadzhiev <P.Hadzhiev96@gmail.com>
2018-10-19 15:23:36 +0300

    Adding a test for the Generator function
    Adding a test for the sample usage of the Generator function, given in the task description.

--- tasks/01/sample_test.go
+++ tasks/01/sample_test.go
@@ -13,6 +13,42 @@ func TestRepeaterSample(t *testing.T) {
 	}
 }
 
+func TestGeneratorSample(t *testing.T) {
+	counter := Generator(
+		func(v int) int { return v + 1 },
+		0,
+	)
+
+	var counterResults [4]int
+	for i := 0; i < 4; i++ {
+		counterResults[i] = counter()
+	}
+
+	actual := counterResults
+	expected := [4]int{0, 1, 2, 3}
+
+	if expected != actual {
+		t.Errorf("Expected `%d` but got `%d`", expected, actual)
+	}
+
+	power := Generator(
+		func(v int) int { return v * v },
+		2,
+	)
+
+	var powerResults [4]int
+	for i := 0; i < 4; i++ {
+		powerResults[i] = power()
+	}
+
+	actual = powerResults
+	expected = [4]int{2, 4, 16, 256}
+
+	if expected != actual {
+		t.Errorf("Expected `%d` but got `%d`", expected, actual)
+	}
+}
+
 func TestMapReducerSample(t *testing.T) {
 	powerSum := MapReducer(
 		func(v int) int { return v * v },
