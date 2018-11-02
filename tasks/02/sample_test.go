493cd490eaec38f61648582851dc26c4ce2a4829
Doychin Atanasov <ironsmile@gmail.com>
2018-11-02 15:45:07 +0200

    Allow mutating editors in the samle test for task 02

--- tasks/02/sample_test.go
+++ tasks/02/sample_test.go
@@ -3,21 +3,23 @@ package main
 import "testing"
 
 func TestSample(t *testing.T) {
-	var f = NewEditor("foobar")
-
 	t.Run("origin", func(t *testing.T) {
+		f := NewEditor("foobar")
 		compare(t, "foobar", f.String())
 	})
 
 	t.Run("insert", func(t *testing.T) {
+		f := NewEditor("foobar")
 		compare(t, "fobazobar", f.Insert(2, "baz").String())
 	})
 
 	t.Run("append", func(t *testing.T) {
+		f := NewEditor("foobar")
 		compare(t, "foobarbaz", f.Insert(6, "baz").String())
 	})
 
 	t.Run("delete", func(t *testing.T) {
+		f := NewEditor("foobar")
 		compare(t, "far", f.Delete(1, 3).String())
 	})
 }
