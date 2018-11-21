a50568a7e33483c7b3265d216ad94ce4bc4e1988
NickyMateev <bgdunkers@gmail.com>
2018-11-21 12:35:36 +0200

    Add additional tests for task03

--- tasks/03/sample_test.go
+++ tasks/03/sample_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/fmi/go-homework/geom"
 )
 
-func TestSampleSimpleOperations(t *testing.T) {
+func TestSampleSimpleOperationsShouldIntersect(t *testing.T) {
 	var prim geom.Intersectable
 
 	a, b, c := geom.NewVector(-1, -1, 0), geom.NewVector(1, -1, 0), geom.NewVector(0, 1, 0)
@@ -15,6 +15,54 @@ func TestSampleSimpleOperations(t *testing.T) {
 
 	if !prim.Intersect(ray) {
 		t.Errorf("Expected ray %#v to intersect triangle %#v but it did not.", ray, prim)
+	}
+}
+
+func TestSampleSimpleTriangleBoundryIntersectionShouldIntersect(t *testing.T) {
+	var prim geom.Intersectable
+
+	a, b, c := geom.NewVector(0, -2, 0), geom.NewVector(3, 0, 0), geom.NewVector(0, 1, 0)
+	prim = NewTriangle(a, b, c)
+	ray := geom.NewRay(geom.NewVector(0, 0, 3), geom.NewVector(0, 0, -2))
+
+	if !prim.Intersect(ray) {
+		t.Errorf("Expected ray %#v to intersect triangle %#v but it did not.", ray, prim)
+	}
+}
+
+func TestSampleSimpleSphereShouldIntersect(t *testing.T) {
+	var prim geom.Intersectable
+
+	origin, r := geom.NewVector(0, 0, 0), 2.0
+	prim = NewSphere(origin, r)
+	ray := geom.NewRay(geom.NewVector(0, 0, 2.5), geom.NewVector(0, 0, 1))
+
+	if !prim.Intersect(ray) {
+		t.Errorf("Expected ray %#v to intersect sphere %#v but it did not.", ray, prim)
+	}
+}
+
+func TestSampleSimpleQuadConvexShouldIntersect(t *testing.T) {
+	var prim geom.Intersectable
+
+	a, b, c, d := geom.NewVector(0, -2, 0), geom.NewVector(3, 0, 0), geom.NewVector(0, 1, 0), geom.Vector{-1, 0, 0}
+	prim = NewQuad(a, b, c, d)
+	ray := geom.NewRay(geom.NewVector(0, 0, 2), geom.NewVector(0, 0, -1))
+
+	if !prim.Intersect(ray) {
+		t.Errorf("Expected ray %#v to intersect quad %#v but it did not.", ray, prim)
+	}
+}
+
+func TestSampleSimpleQuadConcaveShouldNotIntersect(t *testing.T) {
+	var prim geom.Intersectable
+
+	a, b, c, d := geom.NewVector(0, -2, 0), geom.NewVector(3, 0, 0), geom.NewVector(0, 1, 0), geom.Vector{1, 0, 0}
+	prim = NewQuad(a, b, c, d)
+	ray := geom.NewRay(geom.NewVector(0, 0, 2), geom.NewVector(0, 0, -1))
+
+	if prim.Intersect(ray) {
+		t.Errorf("Expected ray %#v to not intersect quad %#v but it did.", ray, prim)
 	}
 }
 
